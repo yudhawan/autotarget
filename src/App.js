d32c8b6aa1e16c1d09fb54efdd4aee6351e8f291
Yudha <yudhaashter@gmail.com>
2022-07-21 13:42:26 +0700

    update

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import {StopIcon,TrashIcon,SwitchVerticalIcon,ClockIcon,PlayIcon,XIcon} from '@heroicons/react/outline'
 import TargetImage from './target.png'
 import TargetImageDown from './target_down.png'
-import { useEffect, useState, } from 'react';
+import { useEffect, useMemo, useState, } from 'react';
 function App() {
   const [target,settarget]=useState([])
   const [play,setplay]=useState(false)
@@ -10,7 +10,7 @@ function App() {
   const [namasesi,setnamasesi] = useState('')
   const [selected,setselected] = useState('')
   const [validation,setvalidation] = useState('')
-  const [ip,setip]=useState(()=>null)
+  const [ip,setip]=useState(null)
   function validationSession(){
     if(target.length<1) return setvalidation('Tidak ada yang bisa disimpan')
     if(save.find(val => val.nama===namasesi)) return setvalidation('Nama sudah ada')
@@ -28,11 +28,7 @@ function App() {
     var x = e.clientX - 20 - e.target.offsetLeft+'px'
     var y = e.clientY - 20 - e.target.offsetTop+'px'
     if(target.length==0) settarget([{
-      id:1,top:y,left:x,status:false,time:null,ip:'', timerRun:()=>{
-        if(this.time){
-          
-        }
-      }
+      id:1,top:y,left:x,status:false,time:null,ip:'', timerRun:null
     }])
     if(target.length>0) {
       const last =target[target.length-1].id
@@ -51,14 +47,30 @@ function App() {
     if(target.id==id) target.status=!target.status
     return target
   }))
-  const playAction = ()=> {
-    setplay(true)
+  let i = 0  
+  
+  function playAction(){
+    setTimeout(()=>{
+      let newdata = target.filter((val)=> val.id!=target[i]?.id)
+      let newrow = target.filter((val)=> val.id==target[i]?.id)[0]
+      newrow.status=true
+      newdata.push(newrow)
+      settarget(newdata)
+      if(i<=target.length-1) {
+        i++
+        // setplay(true)
+        playAction()
+      }
+      // if(i==target.length-1) setplay(false)
+    },1000)
     
   }
   const stopAction = ()=> {
     setplay(false)
-    
   }
+  useEffect(()=>{
+    play&&setInterval(playAction,target.length*1000)
+  },[play])
   return (
   <>
   <div className="lg:px-24 px-4 flex justify-center w-full h-[100vh] bg-black z-10">
@@ -101,7 +113,13 @@ function App() {
                     if(item.id==timer) return settimer(null)
                     return settimer(item.id)
                     }} />
-                  <div className={`rounded-full bg-indigo-400 text-white w-5 h-5 p-1 cursor-pointer text-xs flex justify-center items-center`} onClick={()=>setip(item.id)}>IP</div>
+                  <div className={`rounded-full bg-indigo-400 text-white w-5 h-5 p-1 cursor-pointer text-xs flex justify-center items-center`} onClick={()=>{
+                    if(ip==item.id) {
+                      setip(null)
+                    }else{
+                      setip(item.id)
+                    }
+                    }}>IP</div>
                 </div>
               </div>
             )
@@ -112,23 +130,28 @@ function App() {
       <div className="w-[15%] h-full flex flex-col justify-center items-center space-y-2 py-2">
         <div draggable onDragStart={dragStart} id='0' className='w-24 p-1 h-48 rounded-md shadow-md border-gray-300 border flex flex-col justify-center items-center '>
           <img src={TargetImage} className="bg-cover w-full h-full" />
-          <div className='w-full bg-slate-100 rounded-md flex space-x-1 justify-center p-1'>
+          {/* <div className='w-full bg-slate-100 rounded-md flex space-x-1 justify-center p-1'>
             <SwitchVerticalIcon className={`rounded-full bg-green-400 text-white w-5 h-5 p-1 cursor-pointer`} />
             <ClockIcon className={`rounded-full bg-orange-400 text-white w-5 h-5 p-1 cursor-pointer`} />
             <TrashIcon className={`rounded-full bg-red-400 text-white w-5 h-5 p-1 cursor-pointer`} />
-          </div>
+          </div> */}
         </div>
-        <div className='flex space-x-2 w-full h-40 justify-center items-center border-y border-slate-500 cursor-pointer'>
-          {!play&&<PlayIcon className={`rounded-full bg-indigo-400 text-white w-14 h-14 p-1 `} onClick={playAction}/>}
-          {play&&<StopIcon className={`rounded-full bg-red-400 text-white w-14 h-14 p-1 `} onClick={stopAction}/>}
+        <div className='flex space-x-2 w-full h-40 justify-center items-center border-y border-slate-500'>
+          {!play&&<PlayIcon className={`rounded-full bg-indigo-400 text-white w-14 h-14 p-1 cursor-pointer`} onClick={()=>playAction()}/>}
+          {play&&<StopIcon className={`rounded-full bg-red-400 text-white w-14 h-14 p-1 cursor-pointer`} onClick={stopAction}/>}
           {play?<p className='text-gray-300 text-2xl select-none'>Stop</p>:<p className='text-gray-300 text-2xl select-none'>Play</p>}
           {/* <CogIcon className={`rounded-full bg-gray-400 text-white w-7 h-7 p-1 cursor-pointer`} />   */}
         </div>
         <div className='w-full h-full flex flex-col relative overflow-y-auto px-2 space-y-1'>
-          <p className='underline text-sm text-slate-400 self-end cursor-pointer' onClick={()=>settarget([])}>Clear</p>
+          {target.length>0&&<p className='underline text-sm text-slate-400 self-end cursor-pointer select-none' onClick={()=>settarget([])}>Clear</p>}
           {target?.map(item=>
           <div className='flex justify-between px-2 py-1 rounded-md border border-slate-400 w-full h-auto' >
-            <p className='text-xs font-semibold text-gray-300 select-none cursor-pointer' onClick={()=>setip(item.id)}>Target {item.id}</p>
+            <p className='text-xs font-semibold text-gray-300 select-none cursor-pointer' onClick={()=>{
+              if(ip==item.id) {
+                setip(null)
+              }else{
+                setip(item.id)
+              }}}>Target {item.id}</p>
             <div className='flex space-x-1'>
               {item.status?<SwitchVerticalIcon className={`bg-green-400 rounded-full text-white w-5 h-5 p-1 cursor-pointer`} onClick={()=>switchTarget(item.id)}/>:<SwitchVerticalIcon className={`bg-red-400 rounded-full text-white w-5 h-5 p-1 cursor-pointer`} onClick={()=>switchTarget(item.id)}/>}
               <ClockIcon className={`rounded-full bg-orange-400 text-white w-5 h-5 p-1 cursor-pointer`} onClick={()=>{
@@ -141,7 +164,7 @@ function App() {
         </div>
         <div className='w-full h-[35rem] border-t border-white p-2 flex flex-col items-center space-y-1'>
           <p className='text-white'>Save Session </p>
-          <div className='p-1 rounded-lg border border-violet-200 w-36 bg-white'><input className='outline-none w-full' value={namasesi} placeholder="Nama" type="text" onChange={(e)=> setnamasesi(e.target.value)}/></div>
+          <div className='p-1 rounded-lg border border-violet-200 w-auto bg-white'><input className='outline-none w-full' value={namasesi} placeholder="Nama" type="text" onChange={(e)=> setnamasesi(e.target.value)}/></div>
           {validation&&<p className='text-sm text-red-400'>{validation}</p>}
           <button className='bg-green-400 rounded-lg px-2 text-white font-semibold' onClick={validationSession}>Save</button>
           <div className='mt-1 flex flex-wrap gap-2 w-full'>
@@ -150,7 +173,7 @@ function App() {
                 setselected(val.nama)
                 settarget(val.target)
               }} className={`mt-2 cursor-pointer rounded-lg flex items-center space-x-1 px-2 ${selected===val.nama?'text-white bg-orange-400':'text-gray-500 bg-violet-100'}`}>
-                <p>{val.nama}</p>
+                <p className='select-none'>{val.nama}</p>
                 <XIcon className={`${selected===val.nama?'text-white bg-red-400':'text-gray-500'} rounded-full w-4 h-4`} onClick={()=>deleteSession(val.nama)}/>
                 </div>)
             }
